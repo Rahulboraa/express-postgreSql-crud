c0cb8f03fe15f310ab2e531f3fa55463fe095cf2
Rahul Bora <rahulbora101@gmail.com>
2025-04-26 23:30:03 +0530

    initializedd express server

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 9000;
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+//  Routes
+
+//  Error handling middleware
+
+//  Server running
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is Running on Port ${PORT}`);
+});
